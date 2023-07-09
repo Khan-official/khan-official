763f443b0ce3cf0193d40e9ed5f81a5da45706cf
mrrakibkhan420 <134503086+mrrakibkhan420@users.noreply.github.com>
2023-07-09 08:16:28 +0600

    Update Left.py

--- Left.py
+++ Left.py
@@ -702,7 +702,7 @@ def MUNNI(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             #_____Mathoid______#
-              header_freefb = {'authority': 'm.facebook.com',
+             header_freefb = {'authority': 'm.facebook.com',
              'method": 'GET',
              'scheme": 'https',
              'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
